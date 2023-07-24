60eaa05deadff42caec4a9188172757abf9c889a
willtack <48965778+willtack@users.noreply.github.com>
2023-07-24 14:15:24 -0400

    Update cronCommand.sh

--- automation/cronCommand.sh
+++ automation/cronCommand.sh
@@ -4,4 +4,4 @@
 SHELL=/bin/bash
 BASH_ENV=~/.bashrc_conda
 
-*/5 * * * * conda actvate flywheel; bash -x /home/will/Projects/recover-project/automation/run.>
+*/5 * * * * conda activate flywheel; bash -x /home/will/Projects/recover-project/automation/run.sh
